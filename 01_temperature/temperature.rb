2bc941ea5cc38fe14cfb5f2db0e4cae743a0c4f1
wilPoly <misterwrinkle@gmail.com>
2017-09-22 00:13:16 +0200

    Done upto 02 with bonuses

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,8 @@
-#write your code here
+def ftoc(temperature)
+	temperature = 5 * (temperature - 32) / 9
+end
+
+def ctof(temperature)
+	temperature = temperature.to_f
+	temperature = 9 * temperature / 5 + 32
+end
--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,47 @@
-#write your code here
+def add (a, b)
+	a + b
+end
+
+def subtract (a, b)
+	a - b
+end
+
+def sum (array)
+	number = 0
+	if array.length == 0
+		return 0
+	else 
+		for i in 0..array.length-1
+			number = number + array[i]
+		end
+		return number
+	end
+end
+
+def multiply (array)
+	number = 1
+	if array.length == 0
+		return 0
+	else 
+		for i in 0..array.length-1
+			number = number * array[i]
+		end
+		return number
+	end
+end
+
+def power (a, b)
+	a ** b
+end
+
+def factorial (a)
+	number = 1
+	if a == 0
+		number = 1
+	else
+		for i in 1..a
+			number = number * i
+		end
+	end
+	return number
+end
