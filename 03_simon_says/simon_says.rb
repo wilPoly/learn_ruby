5281dd418150048949767d1782866ea0317c3192
wilPoly <misterwrinkle@gmail.com>
2017-09-24 15:29:57 +0200

    Gone upto 04 >> 'qu' exception to implement

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,37 @@
-#write your code here
+def echo (word)
+	word
+end
+
+def shout (word)
+	word.upcase
+end
+
+def repeat (word, number = 2)
+	array = Array.new(number, word)
+	array = array.join(' ')
+end
+
+def start_of_word (word, number)
+	word[0..number-1]
+end
+
+def first_word (word)
+	array = word.split(' ')
+	array[0].to_s
+end
+
+def titleize (word)
+	word_array = word.split(" ")
+	for i in 0..word_array.length-1
+		if word_array[i] == "and"
+			i += 1
+		elsif  (word_array[i] == "the") and (i != 0)
+			i += 1
+		elsif word_array[i] == "over"
+			i += 1
+		else
+			word_array[i] = word_array[i].capitalize
+		end
+	end
+	word_array.join(" ")
+end
--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,34 @@
-#write your code here
+def translate (word)
+	sentence = word.split #on sépare la phrase en array de mots
+	puts sentence[0]
+	for i in 0..sentence.length-1 #chaque mot est séparé en array de chars
+		sentence[i] = sentence[i].chars
+	end
+		
+	# puts sentence.length
+	# puts sentence[0].length
+
+	for i in 0..sentence.length-1	
+		if sentence[i][0] == "a" or sentence[i][0] == "e" or sentence[i][0] == "i" or sentence[i][0] == "o" or sentence[i][0] == "u"
+			sentence[i].push("ay")
+			i += 1
+		else 
+			while sentence[i][0] != "a" and sentence[i][0] != "e" and sentence[i][0] != "i" and sentence[i][0] != "o" and sentence[i][0] != "u"
+			temp = sentence[i][0]
+			sentence[i].shift
+			sentence[i].push(temp)
+			end
+			sentence[i].push("ay")
+		end
+	end
+
+	translation = []
+	
+	sentence.each_index do |i|
+		translation.push(sentence[i].join)
+	end
+	translation.join(" ")
+	# puts translation
+end
+
+# translate("eat pie")
