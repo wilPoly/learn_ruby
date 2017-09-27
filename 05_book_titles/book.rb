064385eb1c919dd5b0f1a295102aee537e03d86f
wilPoly <misterwrinkle@gmail.com>
2017-09-27 18:56:05 +0200

    Finished 05 - Book titles and refactored

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,19 @@
 class Book
-# write your code here
+
+attr_accessor :title
+
+	def title
+		@title = @title.split	
+		@majuscule = []
+		@exceptions = %w(and in the of a an)
+		@title.each do |i|
+			if @exceptions.include? i
+				@majuscule << i
+			else @majuscule << i.capitalize
+			end
+		end
+		@majuscule.first.capitalize!
+		@title = @majuscule.join(" ")
+	end
+
 end
