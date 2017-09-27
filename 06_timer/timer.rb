c808d8b5bc4809f084dca367df1a4a958f9e99d7
wilPoly <misterwrinkle@gmail.com>
2017-09-27 22:21:11 +0200

    Finished Timer

--- 06_timer/timer.rb
+++ 06_timer/timer.rb
@@ -1,3 +1,27 @@
 class Timer
-  #write your code here
+
+	attr_accessor :seconds
+
+	def seconds
+		@seconds = 0
+	end
+
+	def time_string
+
+		@hours = @seconds / 3600
+		@seconds = @seconds.remainder(3600)
+		@minutes = @seconds / 60
+		@seconds = @seconds.remainder(60)
+
+		if @seconds < 10
+			@seconds = "0#{@seconds}"
+		end
+		if @minutes < 10
+			@minutes = "0#{@minutes}"
+		end
+		if @hours < 10
+			@hours = "0#{@hours}"
+		end
+		@display = "#{@hours}:#{@minutes}:#{@seconds}"
+	end
 end
